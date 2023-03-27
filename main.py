1e827a3888deeef6cddf71dbe55edf3dba76a2cf
17th <17th@inbox.ru>
2023-03-27 13:44:05 +0300

    Добавил работу со скобками

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ operation = {'*': lambda x, y: x * y,
 
 
 def create_expression(count: int) -> str:
-    express = ''.join([str(random.randint(-20, 20)) + str(random.choice(['+', '-', '*', '/'])) for _ in range(count)])[
-              :-1]
+    express = ''.join([str(random.randint(-20, 20)) +
+                       str(random.choice(['+', '-', '*', '/'])) for _ in range(count)])[:-1]
     return express.replace('--', '+').replace('+-', '-')
 
 
@@ -21,7 +21,8 @@ def convert_to_list(exp: str) -> list:
     i = 0
     while i < len(exp):
         if '-' in exp[i][1:]:
-            element = exp.pop(i).replace('-', ' + -').split()
+            element = exp.pop(i)
+            element = element.replace('-', ' + -').split()
             start, finish = exp[:i], exp[i:]
             exp = start + (element[1:] if element[0] == '+' else element) + finish
             i = 0
@@ -30,13 +31,13 @@ def convert_to_list(exp: str) -> list:
     return exp[1:] if exp[0] == '+' else exp
 
 
-def calculate(exp: list) -> int | float:
+def calculate(exp: list, accuracy: int = 2) -> int | float | str:
     while len(exp) > 1:
         if not calculate_operation(exp, ['*', '/']):
             return 'Ошибка! Деление на ноль!'
         calculate_operation(exp, ['+'])
     exp = exp[0]
-    return int(exp) if exp == int(exp) else exp
+    return int(exp) if exp == int(exp) else round(exp, accuracy)
 
 
 def calculate_operation(exp: list, opers: list):
@@ -52,9 +53,40 @@ def calculate_operation(exp: list, opers: list):
     return True
 
 
-for _ in range(20):
-    expression = create_expression(6)
-    eval(expression)
-    exp_list = convert_to_list(expression)
-    print(f'eval = {eval(expression)}')
-    print(f'{exp_list} = {calculate(exp_list)}')
+def check_parentheses(exp: list) -> int | float:
+    while '(' in exp and ')' in exp:
+        deque_bracket = []
+        for i, char in enumerate(exp):
+            if char == '(':
+                deque_bracket.insert(0, i)
+            elif char == ')':
+                start_index = deque_bracket.pop(0)
+                buffered = calculate(exp[start_index + 1:i])
+                if exp[start_index - 1] != '-':
+                    exp = exp[:start_index] + [buffered] + exp[i + 1:]
+                else:
+                    exp = exp[:start_index - 1] + (
+                        [-buffered] if isinstance(exp[start_index - 2],
+                                                  str) else ['+', -buffered]) + exp[i + 1:]
+                break
+    return calculate(exp)
+
+
+def self_eval(exp: str) -> int | float:
+    exp_list = convert_to_list(exp)
+    return check_parentheses(exp_list)
+
+
+# for _ in range(20):
+#     expression = create_expression(6)
+#     print(expression)
+#     eval(expression)
+#     exp_list = convert_to_list(expression)
+#     print(f'eval = {eval(expression)}')
+#     print(f'{exp_list} = {calculate(exp_list, 5)}')
+
+expres = "((2 * 3 - (20 - 5)) - ((3 * (10 - 2))) * (-1))"
+print('eval')
+print(eval(expres))
+print('my_eval:')
+print(f'{expres} = {self_eval(expres)}')
